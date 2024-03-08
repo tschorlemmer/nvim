fc41786ddcbc2668e7fa9450a229bf2ad71134a4
Taylor Schorlemmer <taylor.schorlemmer@gmail.com>
2024-03-08 17:31:24 -0500

    update neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,13 +1,15 @@
 return { {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    priority = 100,
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
     },
     config = function()
-        require 'neo-tree'.setup {}
+        require 'neo-tree'.setup {
+        }
         vim.keymap.set("n", "<leader>d", ":Neotree toggle<CR>")
     end,
 } }
