0443af67dcfe5738151230e6d9b83793f0b20c47
Taylor Schorlemmer <taylor.schorlemmer@gmail.com>
2024-03-25 16:05:10 -0400

    fix harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,7 +8,12 @@ return
         config =
             function()
                 local harpoon = require('harpoon')
-                harpoon:setup()
+                harpoon:setup({
+                    settings = {
+                        save_on_toggle = true,
+                        sync_on_ui_close = true,
+                    }
+                })
                 vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
                 vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
                 vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
