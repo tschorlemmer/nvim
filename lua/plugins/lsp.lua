5cb980584c112063dd66aadd15cbd17743a97ad0
wolf <wolf@dell-laptop.schorlemmer.us>
2024-05-14 01:31:38 -0400

    add default build stuff for jsregxp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,7 +24,10 @@ return {
             },
             "hrsh7th/nvim-cmp",
             "hrsh7th/cmp-nvim-lsp",
-            "L3MON4D3/LuaSnip",
+            {
+                "L3MON4D3/LuaSnip",
+                build = "make install_jsregexp",
+            },
         },
         config =
             function()
